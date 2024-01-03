b1e0de4dc1a2fec5b7ee1493ad57e03ef9f8a8ce
Alex Echavarria <alex.haec.4711@gmail.com>
2024-01-03 19:36:51 +0100

    Add: filter in employee details

--- src/app/components/detail-dialog/detail-dialog.component.ts
+++ src/app/components/detail-dialog/detail-dialog.component.ts
@@ -4,13 +4,18 @@ import {
   MatDialog,
   MatDialogRef,
 } from '@angular/material/dialog';
-import { CompleteEmployee } from '../../entities/employee.entity';
+import { CompleteEmployee, WorkedDays } from '../../entities/employee.entity';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
 import { ConfirmAttendantDialogComponent } from '../confirmAttendantDialog/confirmAttendantDialog.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-detail-dialog',
@@ -23,10 +28,24 @@ import { ConfirmAttendantDialogComponent } from '../confirmAttendantDialog/confi
     MatExpansionModule,
     MatTabsModule,
     MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatMenuModule,
   ],
 })
 export class DetailDialogComponent implements OnInit {
   employee: CompleteEmployee;
+  datesToFilter: { verbose: string; numeral: string }[] = [
+    {
+      verbose: 'Todas',
+      numeral: '0',
+    },
+  ];
+  workedDaysList: WorkedDays[] = [];
+  selectedFilter: any;
 
   constructor(
     public dialogRef: MatDialogRef<DetailDialogComponent>,
@@ -34,14 +53,57 @@ export class DetailDialogComponent implements OnInit {
     private dialog: MatDialog
   ) {
     this.employee = data.employee;
-    console.log(this.employee);
+    this.workedDaysList = this.employee.workedDays;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getDatesToFilter();
+  }
+
+  filterChanged(event: any) {
+    if (event.value.numeral === '0') {
+      this.workedDaysList = this.employee.workedDays;
+      return;
+    }
+
+    this.workedDaysList = this.employee.workedDays.filter((day) =>
+      day.startDate.includes(event.value.numeral)
+    );
+  }
 
   showConfirmAttendandDialog() {
     this.dialog.open(ConfirmAttendantDialogComponent, {
       data: { employee: this.employee },
     });
+  }
+
+  getDatesToFilter() {
+    this.employee.workedDays.map((day) => {
+      const start = day.startDate.split('/');
+      const initMonth = this.nameMonth(Number(start[1]));
+      const initYear = start[2];
+      this.datesToFilter.push({
+        verbose: `${initMonth} ${initYear}`,
+        numeral: `${start[1]}/${start[2]}`,
+      });
+    });
+  }
+
+  nameMonth(monthNumber: number): string {
+    const allMonths = [
+      'Enero',
+      'Febrero',
+      'Marzo',
+      'Abril',
+      'Mayo',
+      'Junio',
+      'Julio',
+      'Agosto',
+      'Septiembre',
+      'Octubre',
+      'Noviembre',
+      'Diciembre',
+    ];
+    return allMonths[monthNumber - 1];
   }
 }
