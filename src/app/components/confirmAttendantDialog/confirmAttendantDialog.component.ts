b9e28443dee43a515b91d21cff61f3be52aec312
Alex Echavarria <alex.haec.4711@gmail.com>
2024-01-05 12:17:25 +0100

    Add: select date and turn in manual attendant is mandatory

--- src/app/components/confirmAttendantDialog/confirmAttendantDialog.component.ts
+++ src/app/components/confirmAttendantDialog/confirmAttendantDialog.component.ts
@@ -12,18 +12,74 @@ import {
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatRadioModule } from '@angular/material/radio';
+import {
+  MatDatepickerIntl,
+  MatDatepickerModule,
+} from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+  MatNativeDateModule,
+} from '@angular/material/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+  MAT_MOMENT_DATE_FORMATS,
+  MomentDateAdapter,
+} from '@angular/material-moment-adapter';
+import 'moment/locale/es';
+import moment from 'moment';
+import { MatIconModule } from '@angular/material/icon';
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-confirmAttendantDialog',
   templateUrl: './confirmAttendantDialog.component.html',
   styleUrls: ['./confirmAttendantDialog.component.css'],
   standalone: true,
-  imports: [MatButtonModule, MatDialogModule, MatRadioModule],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'es' },
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ],
+  imports: [
+    MatButtonModule,
+    MatDialogModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatIconModule,
+  ],
 })
 export class ConfirmAttendantDialogComponent implements OnInit {
   employee: CompleteEmployee;
-  today = new Date().toLocaleDateString();
+  attendantDate = new Date().toLocaleDateString();
   turn: 'morning' | 'afternoon' | 'night' | '' = '';
+  today = new Date();
+  validForm = false;
+
   constructor(
     public dialogRef: MatDialogRef<ConfirmAttendantDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
@@ -36,7 +92,10 @@ export class ConfirmAttendantDialogComponent implements OnInit {
   ngOnInit() {}
 
   confirmAttendant() {
-    if (this.turn === '') return;
+    if (!this.validForm) {
+      return;
+    }
+
     let employee: EmployeeEntity = {} as EmployeeEntity;
     if (this.turn === 'morning') employee = this.buildEmployeeForMorningTurn();
     if (this.turn === 'afternoon')
@@ -57,24 +116,24 @@ export class ConfirmAttendantDialogComponent implements OnInit {
 
   onChange(event: any) {
     this.turn = event.value;
-    console.log(this.turn);
+    this.isValidForm();
   }
 
   buildEmployeeForMorningTurn(): EmployeeEntity {
     const employee: EmployeeEntity = {
       id: this.employee.id,
       complete_name: this.employee.completeName,
-      start_date: this.today,
-      end_date: this.today,
+      start_date: this.attendantDate,
+      end_date: this.attendantDate,
       total_time: JSON.stringify({ hours: 8, minutes: 0 }),
       signings: JSON.stringify([
         {
           hour: '06:00',
-          date: this.today,
+          date: this.attendantDate,
         },
         {
           hour: '14:00',
-          date: this.today,
+          date: this.attendantDate,
         },
       ]),
     };
@@ -86,17 +145,17 @@ export class ConfirmAttendantDialogComponent implements OnInit {
     const employee: EmployeeEntity = {
       id: this.employee.id,
       complete_name: this.employee.completeName,
-      start_date: this.today,
-      end_date: this.today,
+      start_date: this.attendantDate,
+      end_date: this.attendantDate,
       total_time: JSON.stringify({ hours: 8, minutes: 0 }),
       signings: JSON.stringify([
         {
           hour: '14:00',
-          date: this.today,
+          date: this.attendantDate,
         },
         {
           hour: '22:00',
-          date: this.today,
+          date: this.attendantDate,
         },
       ]),
     };
@@ -111,13 +170,13 @@ export class ConfirmAttendantDialogComponent implements OnInit {
     const employee: EmployeeEntity = {
       id: this.employee.id,
       complete_name: this.employee.completeName,
-      start_date: this.today,
+      start_date: this.attendantDate,
       end_date: tomorrow.toLocaleDateString(),
       total_time: JSON.stringify({ hours: 8, minutes: 0 }),
       signings: JSON.stringify([
         {
           hour: '22:00',
-          date: this.today,
+          date: this.attendantDate,
         },
         {
           hour: '06:00',
@@ -127,5 +186,14 @@ export class ConfirmAttendantDialogComponent implements OnInit {
     };
 
     return employee;
+  }
+
+  dateChange(event: any) {
+    this.attendantDate = moment(event.value).format('DD/MM/YYYY');
+    this.isValidForm();
+  }
+
+  isValidForm() {
+    this.validForm = this.attendantDate !== '' && this.turn !== '';
   }
 }
